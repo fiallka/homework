6b4e737b003dab1adc351dddcc6321254c40fd5f
fiallka <a.vanintseva@yandex.ru>
2025-09-30 02:59:02 +0300

    wake-bake

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -1,18 +1,48 @@
-(function() {
+(function () {
     document.addEventListener("DOMContentLoaded", () => {
+        const body = document.body;
+        const burger = document.querySelector(".burger-icon");
+        const navLinks = document.querySelectorAll(".nav__link");
+
+        burger.addEventListener("click", () => {
+            body.classList.toggle("body--opened-menu");
+        });
+
+        navLinks.forEach(link => {
+            link.addEventListener("click", () => {
+                body.classList.remove("body--opened-menu");
+            });
+        });
+    });
+
+    const btn = document.querySelector('.about__img-button')
+    const modal = document.querySelector('.modal')
     const body = document.body;
-    const burger = document.querySelector(".burger-icon");
-    const navLinks = document.querySelectorAll(".nav__link");
 
-    burger.addEventListener("click", () => {
-    body.classList.toggle("body--opened-menu");
-    });
+    const openModal = () => {
+        body.classList.add('body--opened-modal')
+    }
+    const closeModal = () => {
+        body.classList.remove('body--opened-modal')
+    }
 
-    navLinks.forEach(link => {
-    link.addEventListener("click", () => {
-        body.classList.remove("body--opened-menu");
-    });
-    });
-});
+    btn.addEventListener('click', openModal)
+
+    modal.addEventListener('click', event => {
+        const target = event.target
+        if (target.classList.contains('modal') || target.closest('.modal__cancel')) {
+            closeModal()
+        }
+    })
+    document.addEventListener('keydown', event => {
+        if (event.code === 'Escape') {
+            closeModal();
+        }
+    }
+    )
+//====== 
 })()
+
+
+
 
