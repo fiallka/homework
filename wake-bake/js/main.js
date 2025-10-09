6563d773c9ff388856e1a6e3c1006f70b46a42ca
fiallka <a.vanintseva@yandex.ru>
2025-10-09 21:38:46 +0300

    wake-bake

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -156,9 +156,10 @@
         }
         
     });
-
-
-    // 
+    // маска для телефона
+    const telInputs = document.querySelectorAll('input[type="tel"]')
+    const im = new Inputmask('+7 (999) 999-99-99')
+    im.mask(telInputs)
 })()
 
 
