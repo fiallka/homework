dbe681cfc65c929697473b9e5d7dee11497abbcd
fiallka <a.vanintseva@yandex.ru>
2025-10-14 01:02:02 +0300

    Pulse_Fitness Center

--- Pulse_Fitness Center/js/main.js
+++ Pulse_Fitness Center/js/main.js
@@ -29,5 +29,58 @@
             },
         },
     });
+    // ----------------tabs
+    document.querySelectorAll('.tabs-wrapper').forEach(wrapper => {
+        const buttons = wrapper.querySelectorAll('.tab-btn');
+        const contents = wrapper.querySelectorAll('.tab-content');
 
+        buttons.forEach(btn => {
+            btn.addEventListener('click', () => {
+                const tabId = btn.dataset.tab;
+
+                // Активируем кнопку
+                buttons.forEach(b => b.classList.remove('active'));
+                btn.classList.add('active');
+
+                // Активируем контент
+                contents.forEach(content => {
+                    content.classList.remove('active');
+                    if (content.dataset.tab === tabId) {
+                        content.classList.add('active');
+                    }
+                });
+            });
+        });
+    });
+    // Accordion
+    const accordionLists = document.querySelectorAll('.accordion-list')
+
+    accordionLists.forEach(eL => {
+
+        eL.addEventListener('click', (e) => {
+
+            const accordionList = e.currentTarget
+            const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened');
+            const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content');
+
+            const accordionControl = e.target.closest('.accordion-list__control');
+            if (!accordionControl) return
+            e.preventDefault()
+            const accordionItem = accordionControl.parentElement;
+            const accordionContent = accordionControl.nextElementSibling;
+
+            if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+                accordionOpenedItem.classList.remove('accordion-list__item--opened');
+                accordionOpenedContent.style.maxHeight = null;
+            }
+
+            accordionItem.classList.toggle('accordion-list__item--opened');
+
+            if (accordionItem.classList.contains('accordion-list__item--opened')) {
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+            } else {
+                accordionContent.style.maxHeight = null;
+            }
+        });
+    });
 })()
